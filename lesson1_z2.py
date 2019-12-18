fb8cf2e3d774317e8504af01f6974ea5faf87df4
Mark Anna <anna.mark.spb@gmail.com>
2019-12-18 11:17:19 +0300

    1-4 задания доработки

--- lesson1_z2.py
+++ lesson1_z2.py
@@ -1,8 +1,19 @@
 # Задание 2
+
 time_sec = int(input('Введите время в секундах\n'))
-hour = time_sec//(60*60)
-minites = time_sec//60
-sec = time_sec - minites*60 - hour*60
-print(hour, minites, sec, sep=':')
+mm = time_sec%60**2
+print(time_sec//60**2, mm//60, mm%60, sep=':')
 # или так
-print(f'{hour}:{minites}:{sec}')
+hh = time_sec//60**2
+ss = mm%60
+print(f'{hh}:{mm//60}:{ss}')
+# если нужен формат 02:02:02
+if hh <= 9:
+    hh = f'0{hh}'
+if mm//60 <= 9:
+    mm = f'0{mm//60}'
+else:
+    mm = mm//60
+if ss <= 9:
+    ss = f'0{ss}'
+print(f'{hh}:{mm}:{ss}')
--- lesson1_z4.py
+++ lesson1_z4.py
@@ -1,16 +1,8 @@
 # Задание 4
-number = input('Введите целое положительное число\n')
-n = len(number)
-n_list = [int(i) for i in number]
-n_max = max(n_list)
-print(n_max)
-# что-то у меня с математикой плохо -_-
-# а если подумать...
+
 number = int(input('Введите целое положительное число\n'))
 x = 0
 while number > 0:
-    ost = number%10
+    x = number%10 if x < number%10 else x
     number = number//10
-    if x < ost:
-        x = ost
 print(x)
