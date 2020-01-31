3890626a5bd06a2196575c016d95e7e0dd0d3534
Mark Anna <anna.mark.spb@gmail.com>
2020-02-01 00:39:46 +0300

    дз8.1

--- lesson08.py
+++ lesson08.py
@@ -1,84 +1,84 @@
-# """ Задание 1 """
-#
-#
-# class DateError(Exception):
-#     def __init__(self, text=None):
-#         self.text = text
-#
-#     def __str__(self):
-#         return 'Дата не существует'
-#
-#
-# class Date:
-#     def __init__(self, date):
-#         self.date = date
-#
-#     @classmethod
-#     def date_to_int(cls, date):
-#         date_split = cls(date).date.split('-')
-#         return tuple(map(int, date_split))
-#
-#     @staticmethod
-#     def validation(date):
-#         a, b, c = Date.date_to_int(date)
-#         if not ((b == 2 and c%4 == 0 and a in range(1, 30)) or
-#                 (b == 2 and c%4 != 0 and a in range(1, 29)) or
-#                 (b in range(1, 13, 2) and a in range(1, 32)) or
-#                 (b in range(4, 13, 2) and a in range(1, 31))):
-#             raise DateError()
-#
-#
-# print(Date.date_to_int('21-09-2019'))
-# try:
-#     Date.validation('31-12-2020')
-# except DateError as e:
-#     print(e)
-#
-# """ Задание 2 """
-#
-#
-# class ZeroError(Exception):
-#     def __init__(self, text=None):
-#         self.text = text
-#
-#     def __str__(self):
-#         return 'деление на 0'
-#
-#
-# a = int(input('а: '))
-# try:
-#     b = int(input('b: '))
-#     if b == 0:
-#         raise ZeroError()
-#     else:
-#         print(a/b)
-# except ZeroError as e:
-#     print(e)
-#
-#
-# """ Задание 3 """
-#
-# import re
-#
-# class ElementNotNumber(Exception):
-#     def __init__(self, text=None):
-#         self.text = text
-#     def __str__(self):
-#         return 'Не число'
-#
-# list_n = []
-# while True:
-#     el = input('Чтобы закончить введите "stop" \nВвод: ')
-#     if el == 'stop':
-#         break
-#     try:
-#         if re.search('[^\d.]', el):
-#             raise ElementNotNumber()
-#     except ElementNotNumber as e:
-#         print(e)
-#     else:
-#         list_n.append(float(el) if '.' in el else int(el))
-# print(list_n)
+""" Задание 1 """
+
+
+class DateError(Exception):
+    def __init__(self, text=None):
+        self.text = text
+
+    def __str__(self):
+        return 'Дата не существует'
+
+
+class Date:
+    def __init__(self, date):
+        self.date = date
+
+    @classmethod
+    def date_to_int(cls, date):
+        date_split = cls(date).date.split('-')
+        return tuple(map(int, date_split))
+
+    @staticmethod
+    def validation(date):
+        a, b, c = Date.date_to_int(date)
+        if not ((b == 2 and c%4 == 0 and a in range(1, 30)) or
+                (b == 2 and c%4 != 0 and a in range(1, 29)) or
+                (b in range(1, 13, 2) and a in range(1, 32)) or
+                (b in range(4, 13, 2) and a in range(1, 31))):
+            raise DateError()
+
+
+print(Date.date_to_int('21-09-2019'))
+try:
+    Date.validation('31-12-2020')
+except DateError as e:
+    print(e)
+
+""" Задание 2 """
+
+
+class ZeroError(Exception):
+    def __init__(self, text=None):
+        self.text = text
+
+    def __str__(self):
+        return 'деление на 0'
+
+
+a = int(input('а: '))
+try:
+    b = int(input('b: '))
+    if b == 0:
+        raise ZeroError()
+    else:
+        print(a/b)
+except ZeroError as e:
+    print(e)
+
+
+""" Задание 3 """
+
+import re
+
+class ElementNotNumber(Exception):
+    def __init__(self, text=None):
+        self.text = text
+    def __str__(self):
+        return 'Не число'
+
+list_n = []
+while True:
+    el = input('Чтобы закончить введите "stop" \nВвод: ')
+    if el == 'stop':
+        break
+    try:
+        if re.search('[^\d.]', el):
+            raise ElementNotNumber()
+    except ElementNotNumber as e:
+        print(e)
+    else:
+        list_n.append(float(el) if '.' in el else int(el))
+print(list_n)
 
 """ Задание 4,5,6 """
 
